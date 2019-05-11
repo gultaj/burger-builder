9b97d43d5a368eeab8b4d31799d9c94e36880649
Oleg <gultaj@users.noreply.github.com>
2019-05-11 17:49:20 +0300

    Delete Google API keys

--- src/axios-auth.js
+++ src/axios-auth.js
@@ -3,7 +3,6 @@ import axios from 'axios';
 const instance = axios.create({
   baseURL: 'https://www.googleapis.com/identitytoolkit/v3/relyingparty',
   params: {
-    //   key: 'AIzaSyCW14faVxPQv4VDzUwFxeF8HIJ2u9qxjmI'
     key: process.env.REACT_APP_FIREBASE_API_KEY
   }
 });
